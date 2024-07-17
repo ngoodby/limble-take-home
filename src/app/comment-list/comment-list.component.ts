5bb89167f5bc0f28ae8fae7994c0d383e4767334
Nathaniel Goodby <nathaniel.goodby@dfw.wa.gov>
2024-07-17 14:17:19 -0700

    add font awesome icon to comments

--- src/app/comment-list/comment-list.component.ts
+++ src/app/comment-list/comment-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { CommentService } from '../comment.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'comment-list',
   templateUrl: './comment-list.component.html',
   styleUrls: ['./comment-list.component.css'],
   standalone: true,
+  imports: [FontAwesomeModule],
 })
 export default class AppComponent {
   commentService = inject(CommentService);
+  faComment=faComment
 }
