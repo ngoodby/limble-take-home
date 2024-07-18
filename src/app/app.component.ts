39b1e5cf24051c842a461189705d8021562d4fe1
Nathaniel Goodby <nathaniel.goodby@dfw.wa.gov>
2024-07-18 13:01:42 -0700

    remove RouterOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
 import CommentFormComponent from './comment-form/comment-form.component';
 import CommentListComponent from './comment-list/comment-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommentFormComponent, CommentListComponent],
+  imports: [CommentFormComponent, CommentListComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
